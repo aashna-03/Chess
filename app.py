1f85d0ba8476bb5cebbfbc28659860e9a9574991
aashna-03 <ferroaash@gmail.com>
2024-09-09 14:18:48 +0530

    pieces added to the board

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import pygame
 pygame.init()
-<<<<<<< HEAD
 WIDTH=1500
-=======
-WIDTH=1200
->>>>>>> ec246c91c9e096925e614aa7d177c75014e79309
 HEIGHT=800
 screen=pygame.display.set_mode([WIDTH,HEIGHT])
 pygame.display.set_caption('Two-player Chess')
@@ -13,19 +9,11 @@ big_font=pygame.font.SysFont('freesansbold.ttt',40)
 timer=pygame.time.Clock()
 fps=60
 
-<<<<<<< HEAD
 white_pieces=['rook','knight','bishop','king','queen','bishop','knight','rook','pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn']
 white_locations=[(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),
                  (0,1),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1)]
 
 black_pieces=['rook','knight','bishop','king','queen','bishop','knight','rook','pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn']
-=======
-white_pieces={'rook','knight','bishop','king','queen','bishop','knight','rook','pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn'}
-white_locations=[(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),
-                 (0,1),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1)]
-
-black_pieces={'rook','knight','bishop','king','queen','bishop','knight','rook','pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn'}
->>>>>>> ec246c91c9e096925e614aa7d177c75014e79309
 black_locations=[(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7),
                  (0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(7,6)]
 capture_pieces_white=[]
@@ -35,7 +23,6 @@ turn_step=0
 selection=100
 valid_moves=[]
 
-<<<<<<< HEAD
 black_queen=pygame.image.load('A:/chess ai/pieces/images/black queen.png')
 black_queen=pygame.transform.scale(black_queen,(80,80))
 black_queen_small=pygame.transform.scale(black_queen,(45,45))
@@ -49,21 +36,6 @@ black_king=pygame.transform.scale(black_king,(80,80))
 black_king_small=pygame.transform.scale(black_king,(45,45))
 
 white_king=pygame.image.load('A:\chess ai\pieces\images\white king.png')
-=======
-black_queen=pygame.image.load('A:/chess ai/pieces/images/black queen.jpg')
-black_queen=pygame.transform.scale(black_queen,(80,80))
-black_queen_small=pygame.transform.scale(black_queen,(45,45))
-
-white_queen=pygame.image.load('A:\chess ai\pieces\images\white queen.jpg')
-white_queen=pygame.transform.scale(white_queen,(80,80))
-white_queen_small=pygame.transform.scale(white_queen,(45,45))
-
-black_king=pygame.image.load('A:\chess ai\pieces\images\white king.jpg')
-black_king=pygame.transform.scale(black_king,(80,80))
-black_king_small=pygame.transform.scale(black_king,(45,45))
-
-white_king=pygame.image.load('A:\chess ai\pieces\images\white queen.jpg')
->>>>>>> ec246c91c9e096925e614aa7d177c75014e79309
 white_king=pygame.transform.scale(white_king,(80,80))
 white_king_small=pygame.transform.scale(white_king,(45,45))
 
@@ -71,11 +43,7 @@ black_rook=pygame.image.load('A:/chess ai/pieces/images/black rook.png')
 black_rook=pygame.transform.scale(black_rook,(80,80))
 black_rook_small=pygame.transform.scale(black_rook,(45,45))
 
-<<<<<<< HEAD
 white_rook=pygame.image.load('A:\chess ai\pieces\images\white rook.png')
-=======
-white_rook=pygame.image.load('A:\chess ai\pieces\images\white rook.jpg')
->>>>>>> ec246c91c9e096925e614aa7d177c75014e79309
 white_rook=pygame.transform.scale(white_rook,(80,80))
 white_rook_small=pygame.transform.scale(white_rook,(45,45))
 
@@ -87,7 +55,6 @@ white_bishop=pygame.image.load('A:\chess ai\pieces\images\white bishop.png')
 white_bishop=pygame.transform.scale(white_bishop,(80,80))
 white_bishop_small=pygame.transform.scale(white_bishop,(45,45))
 
-<<<<<<< HEAD
 black_knight=pygame.image.load('A:/chess ai/pieces/images/black knight.png')
 black_knight=pygame.transform.scale(black_knight,(80,80))
 black_knight_small=pygame.transform.scale(black_knight,(45,45))
@@ -101,26 +68,10 @@ black_pawn=pygame.transform.scale(black_pawn,(80,80))
 black_pawn_small=pygame.transform.scale(black_pawn,(45,45))
 
 white_pawn=pygame.image.load('A:\chess ai\pieces\images\white pawn.png')
-=======
-black_knight=pygame.image.load('A:/chess ai/pieces/images/black knight.jpg')
-black_knight=pygame.transform.scale(black_knight,(80,80))
-black_knight_small=pygame.transform.scale(black_knight,(45,45))
-
-white_knight=pygame.image.load('A:\chess ai\pieces\images\white bishop.png')
-white_knight=pygame.transform.scale(white_knight,(80,80))
-white_knight_small=pygame.transform.scale(white_knight,(45,45))
-
-black_pawn=pygame.image.load('A:/chess ai/pieces/images/black pawn.jpg')
-black_pawn=pygame.transform.scale(black_pawn,(80,80))
-black_pawn_small=pygame.transform.scale(black_pawn,(45,45))
-
-white_pawn=pygame.image.load('A:\chess ai\pieces\images\white pawn.jpg')
->>>>>>> ec246c91c9e096925e614aa7d177c75014e79309
 white_pawn=pygame.transform.scale(white_pawn,(80,80))
 white_pawn_small=pygame.transform.scale(white_pawn,(45,45))
 
 
-<<<<<<< HEAD
 white_images = [white_pawn, white_rook, white_knight, white_bishop, white_queen, white_king]
 small_white_images = [white_pawn_small, white_rook_small, white_knight_small, white_bishop_small, white_queen_small, white_king_small]
 
@@ -128,28 +79,14 @@ black_images = [black_pawn, black_rook, black_knight, black_bishop, black_queen,
 small_black_images = [black_pawn_small, black_rook_small, black_knight_small, black_bishop_small, black_queen_small, black_king_small]
 
 piece_list = ['pawn', 'rook', 'knight', 'bishop', 'queen', 'king'] #according to indexing given to images list
-=======
-white_images=[white_pawn,white_queen,white_king,white_knight,white_bishop,white_rook]
-small_white_images=[white_pawn_small,white_queen_small,white_king_small,white_knight_small,white_bishop_small,white_rook_small]
-
-black_images=[black_pawn,black_queen,black_king,black_knight,black_bishop,black_rook]
-small_black_images=[black_pawn_small,black_queen_small,black_king_small,black_knight_small,black_bishop_small,black_rook_small]
-
-piece_list=['pawn','queen','king','knight','rook','bishop'] #according to indexing given to images list
->>>>>>> ec246c91c9e096925e614aa7d177c75014e79309
 
 WHITE = (255, 255, 255)
 DARK_GREEN= (100, 200, 50)
 
 
 def draw_board():
-<<<<<<< HEAD
     for row in range(9):
         for column in range(9):
-=======
-    for row in range(8):
-        for column in range(8):
->>>>>>> ec246c91c9e096925e614aa7d177c75014e79309
         
          if (row + column) % 2:
          
@@ -159,7 +96,6 @@ def draw_board():
         
         color=DARK_GREEN
         pygame.draw.rect(screen, color, [column * 100, row * 100, 100, 100])
-<<<<<<< HEAD
         rect = pygame.Rect(800, 0, 200, 800)
         pygame.draw.rect(screen, 'gold',rect,width=3)
         
@@ -186,21 +122,6 @@ def draw_pieces():
        screen.blit(black_pawn,(black_locations[i][0]*100,black_locations[i][1]*100))
       else:
          screen.blit(black_images[index],(black_locations[i][0]*100,black_locations[i][1]*100))
-=======
-        rect = pygame.Rect(700, 0, 200, 800)
-        pygame.draw.rect(screen, 'gold',rect,width=3)
-        
-
-         
-         
-        rect = pygame.Rect(900, 250, 300, 400)
-        pygame.draw.rect(screen, 'black',rect,width=5) 
-               
-         
-
-
-        
->>>>>>> ec246c91c9e096925e614aa7d177c75014e79309
 
 
 
@@ -210,17 +131,11 @@ while run:
 
     screen.fill('dark green')
     draw_board()
-<<<<<<< HEAD
     draw_pieces()
-=======
->>>>>>> ec246c91c9e096925e614aa7d177c75014e79309
 
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
     pygame.display.flip()
 pygame.quit()
-<<<<<<< HEAD
 
-=======
->>>>>>> ec246c91c9e096925e614aa7d177c75014e79309
