1aa925bc77d4bb6617d97b35d7d5b78547100019
aashna-03 <ferroaash@gmail.com>
2026-05-06 09:55:23 +0530

    fix:App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../src/pages/home.jsx";
-import Game from "../src/pages/game.jsx";
+import Home from "../src/pages/Home.jsx";
+import Game from "../src/pages/Game.jsx";
 
 export default function App() {
     return (
