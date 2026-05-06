4e38e50a35c41faa36e66e248ac3e3a941fccbee
aashna-03 <ferroaash@gmail.com>
2026-05-06 10:16:32 +0530

    fix: File names

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import ChessBoard from "../components/chess/ChessBoard";
-import { useChessGame } from "../hooks/useWebSocket";
+import { useChessGame } from "../hooks/useWebsocket";
 
 const PIECE_EMOJI = {
     pawn: "♟", knight: "♞", bishop: "♝",
