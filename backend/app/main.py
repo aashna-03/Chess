f753d598fb1277e3356ebc3f0a93e5731fccfcd1
aashna-03 <ferroaash@gmail.com>
2026-05-06 11:34:59 +0530

    fix: Fix websocket connection error

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,16 +3,19 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routers import game, analysis, lichess, chesscom
 from app.core.config import settings
 
-app = FastAPI(title="Chess App", version="1.0.0")
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","gambitai.vercel.app", settings.FRONTEND_URL],
+    allow_origins=[
+        "https://gambitai.vercel.app",
+        "http://localhost:5173",
+        settings.FRONTEND_URL
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(lichess.router, prefix="/lichess", tags=["lichess"])
 app.include_router(chesscom.router, prefix="/chesscom", tags=["chesscom"])
 app.include_router(game.router, prefix="/game", tags=["game"])
